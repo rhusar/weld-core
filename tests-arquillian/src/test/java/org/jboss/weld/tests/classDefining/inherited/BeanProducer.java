weld/core

WELD-2666 Reproducer and potential fix for proxy naming of hierarchical interface-based beans.

tests-arquillian/src/test/java/org/jboss/weld/tests/classDefining/inherited/BeanProducer.java
@@ -1,0 +1,56 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2021, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.weld.tests.classDefining.inherited;
+
+import org.jboss.weld.tests.classDefining.inherited.extending.MyInterface;
+import org.jboss.weld.tests.classDefining.inherited.base.AncestorInterface;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+
+@ApplicationScoped
+public class BeanProducer {
+
+    // produce a proxied bean for a type with interface hierarchy
+    @Produces
+    @ApplicationScoped
+    public MyInterface produceBean() {
+        return new MyInterface() {
+            @Override
+            public String anotherPing() {
+                return MyInterface.class.getSimpleName();
+            }
+
+            @Override
+            public String ping() {
+                return AncestorInterface.class.getSimpleName();
+            }
+        };
+    }
+
+    @Produces
+    @ApplicationScoped
+    public AMuchBetterPrincipal producePrincipal() {
+        return new AMuchBetterPrincipal() {
+            @Override
+            public String getName() {
+                return AMuchBetterPrincipal.class.getSimpleName();
+            }
+        };
+    }
+}

tests-arquillian/src/test/java/org/jboss/weld/tests/classDefining/inherited/ConsumerBean.java
@@ -1,0 +1,41 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2021, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.weld.tests.classDefining.inherited;
+
+import org.jboss.weld.tests.classDefining.inherited.extending.MyInterface;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+@ApplicationScoped
+public class ConsumerBean {
+
+    @Inject
+    MyInterface bean;
+
+    @Inject
+    AMuchBetterPrincipal principal;
+
+    public MyInterface getProducedInterfaceBean() {
+        return bean;
+    }
+
+    public AMuchBetterPrincipal getPrincipal() {
+        return principal;
+    }
+}

tests-arquillian/src/test/java/org/jboss/weld/tests/classDefining/inherited/ProxyForHierarchicalInterfaceTypeTest.java
@@ -1,0 +1,71 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2021, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.weld.tests.classDefining.inherited;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.BeanArchive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.weld.test.util.Utils;
+import org.jboss.weld.tests.classDefining.inherited.base.AncestorInterface;
+import org.jboss.weld.tests.classDefining.inherited.extending.MyInterface;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+
+/**
+ * Tests that you can create a proxy (specifically under JDK 11) from producer that returns a hierarchical interface
+ * type. E.g. tests that the resulting proxy name has package corresponding to its class name.
+ *
+ * One of the proxies is built on an interface extending Principal, this is deliberate as that lies in java.* package
+ * which gets special treatment.
+ */
+@RunWith(Arquillian.class)
+public class ProxyForHierarchicalInterfaceTypeTest {
+
+    @Deployment
+    public static Archive<?> deploy() {
+        return ShrinkWrap.create(BeanArchive.class, Utils.getDeploymentNameAsHash(ProxyForHierarchicalInterfaceTypeTest.class))
+                .addClass(AncestorInterface.class)
+                .addClass(MyInterface.class)
+                .addClass(ConsumerBean.class)
+                .addClass(BeanProducer.class)
+                .addClass(AMuchBetterPrincipal.class);
+    }
+
+    @Inject
+    ConsumerBean bean;
+
+    @Test
+    public void testProxyDefinitionWorks() {
+        // invoke the method, the verification lies mainly in not getting errors when creating proxy
+        MyInterface interfaceBean = this.bean.getProducedInterfaceBean();
+        Assert.assertEquals(MyInterface.class.getSimpleName(), interfaceBean.anotherPing());
+        Assert.assertEquals(AncestorInterface.class.getSimpleName(), interfaceBean.ping());
+        // assert that the proxy from hierarchical interface start with package based on alphabetical ordering of partaking classes
+        Assert.assertTrue(interfaceBean.getClass().getName().startsWith("org.jboss.weld.tests.classDefining.inherited.base.AncestorInterface"));
+
+        AMuchBetterPrincipal principal = this.bean.getPrincipal();
+        Assert.assertEquals(AMuchBetterPrincipal.class.getSimpleName(), principal.getName());
+        // assert that the proxy created from Principal and custom class has the package of custom class even though it is alphabetically reversed
+        Assert.assertTrue(principal.getClass().getName().startsWith("org.jboss.weld.tests.classDefining.inherited.AMuchBetterPrincipal"));
+    }
+}

rhusar/weld-core

tests-arquillian/src/test/java/org/jboss/weld/tests/classDefining/inherited/BeanProducer.java
@@ -1,0 +1,56 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2021, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.weld.tests.classDefining.inherited;
+
+import org.jboss.weld.tests.classDefining.inherited.extending.MyInterface;
+import org.jboss.weld.tests.classDefining.inherited.base.AncestorInterface;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Produces;
+
+@ApplicationScoped
+public class BeanProducer {
+
+    // produce a proxied bean for a type with interface hierarchy
+    @Produces
+    @ApplicationScoped
+    public MyInterface produceBean() {
+        return new MyInterface() {
+            @Override
+            public String anotherPing() {
+                return MyInterface.class.getSimpleName();
+            }
+
+            @Override
+            public String ping() {
+                return AncestorInterface.class.getSimpleName();
+            }
+        };
+    }
+
+    @Produces
+    @ApplicationScoped
+    public AMuchBetterPrincipal producePrincipal() {
+        return new AMuchBetterPrincipal() {
+            @Override
+            public String getName() {
+                return AMuchBetterPrincipal.class.getSimpleName();
+            }
+        };
+    }
+}

tests-arquillian/src/test/java/org/jboss/weld/tests/classDefining/inherited/ConsumerBean.java
@@ -1,0 +1,41 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2021, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.weld.tests.classDefining.inherited;
+
+import org.jboss.weld.tests.classDefining.inherited.extending.MyInterface;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+@ApplicationScoped
+public class ConsumerBean {
+
+    @Inject
+    MyInterface bean;
+
+    @Inject
+    AMuchBetterPrincipal principal;
+
+    public MyInterface getProducedInterfaceBean() {
+        return bean;
+    }
+
+    public AMuchBetterPrincipal getPrincipal() {
+        return principal;
+    }
+}

tests-arquillian/src/test/java/org/jboss/weld/tests/classDefining/inherited/ProxyForHierarchicalInterfaceTypeTest.java
@@ -1,0 +1,71 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2021, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.weld.tests.classDefining.inherited;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.BeanArchive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.weld.test.util.Utils;
+import org.jboss.weld.tests.classDefining.inherited.base.AncestorInterface;
+import org.jboss.weld.tests.classDefining.inherited.extending.MyInterface;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import jakarta.inject.Inject;
+
+/**
+ * Tests that you can create a proxy (specifically under JDK 11) from producer that returns a hierarchical interface
+ * type. E.g. tests that the resulting proxy name has package corresponding to its class name.
+ *
+ * One of the proxies is built on an interface extending Principal, this is deliberate as that lies in java.* package
+ * which gets special treatment.
+ */
+@RunWith(Arquillian.class)
+public class ProxyForHierarchicalInterfaceTypeTest {
+
+    @Deployment
+    public static Archive<?> deploy() {
+        return ShrinkWrap.create(BeanArchive.class, Utils.getDeploymentNameAsHash(ProxyForHierarchicalInterfaceTypeTest.class))
+                .addClass(AncestorInterface.class)
+                .addClass(MyInterface.class)
+                .addClass(ConsumerBean.class)
+                .addClass(BeanProducer.class)
+                .addClass(AMuchBetterPrincipal.class);
+    }
+
+    @Inject
+    ConsumerBean bean;
+
+    @Test
+    public void testProxyDefinitionWorks() {
+        // invoke the method, the verification lies mainly in not getting errors when creating proxy
+        MyInterface interfaceBean = this.bean.getProducedInterfaceBean();
+        Assert.assertEquals(MyInterface.class.getSimpleName(), interfaceBean.anotherPing());
+        Assert.assertEquals(AncestorInterface.class.getSimpleName(), interfaceBean.ping());
+        // assert that the proxy from hierarchical interface start with package based on alphabetical ordering of partaking classes
+        Assert.assertTrue(interfaceBean.getClass().getName().startsWith("org.jboss.weld.tests.classDefining.inherited.base.AncestorInterface"));
+
+        AMuchBetterPrincipal principal = this.bean.getPrincipal();
+        Assert.assertEquals(AMuchBetterPrincipal.class.getSimpleName(), principal.getName());
+        // assert that the proxy created from Principal and custom class has the package of custom class even though it is alphabetically reversed
+        Assert.assertTrue(principal.getClass().getName().startsWith("org.jboss.weld.tests.classDefining.inherited.AMuchBetterPrincipal"));
+    }
+}