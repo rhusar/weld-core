weld/core

WELD-2658 Fire ProcessBean event for enabled synthetic alternatives.

impl/src/main/java/org/jboss/weld/util/Beans.java
@@ -58,6 +58,7 @@ import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.BeanAttributes;
 import javax.enterprise.inject.spi.Extension;
 import javax.enterprise.inject.spi.PassivationCapable;
+import javax.enterprise.inject.spi.Prioritized;
 import javax.inject.Inject;
 
 import org.jboss.weld.annotated.enhanced.EnhancedAnnotated;
@@ -70,6 +71,7 @@ import org.jboss.weld.bean.DecoratorImpl;
 import org.jboss.weld.bean.ForwardingBean;
 import org.jboss.weld.bean.InterceptorImpl;
 import org.jboss.weld.bean.RIBean;
+import org.jboss.weld.bean.WeldBean;
 import org.jboss.weld.bootstrap.api.ServiceRegistry;
 import org.jboss.weld.bootstrap.enablement.ModuleEnablement;
 import org.jboss.weld.injection.FieldInjectionPoint;
@@ -223,16 +225,23 @@ public class Beans {
 
     public static boolean isBeanEnabled(Bean<?> bean, ModuleEnablement enabled) {
         if (bean.isAlternative()) {
+            boolean isEnabled = false;
             if (enabled.isEnabledAlternativeClass(bean.getBeanClass())) {
-                return true;
+                isEnabled = true;
             } else {
                 for (Class<? extends Annotation> stereotype : bean.getStereotypes()) {
                     if (enabled.isEnabledAlternativeStereotype(stereotype)) {
-                        return true;
+                        isEnabled = true;
+                        break;
                     }
                 }
-                return false;
             }
+            // For synthetic enabled alternatives, the ModuleEnablement may not yet be aware of them
+            if (!isEnabled
+                    && ((bean instanceof WeldBean && ((WeldBean<?>) bean).getPriority() != null) || bean instanceof Prioritized)) {
+                isEnabled = true;
+            }
+            return isEnabled;
         } else if (bean instanceof AbstractProducerBean<?, ?, ?>) {
             AbstractProducerBean<?, ?, ?> receiverBean = (AbstractProducerBean<?, ?, ?>) bean;
             return isBeanEnabled(receiverBean.getDeclaringBean(), enabled);

tests-arquillian/src/test/java/org/jboss/weld/tests/alternatives/customBeanPriority/MyExtension.java
@@ -22,6 +22,7 @@ import javax.enterprise.inject.Any;
 import javax.enterprise.inject.Default;
 import javax.enterprise.inject.spi.BeanManager;
 import javax.enterprise.inject.spi.Extension;
+import javax.enterprise.inject.spi.ProcessSyntheticBean;
 
 import org.jboss.weld.bootstrap.event.WeldAfterBeanDiscovery;
 
@@ -30,6 +31,8 @@ import org.jboss.weld.bootstrap.event.WeldAfterBeanDiscovery;
  * @author <a href="mailto:manovotn@redhat.com">Matej Novotny</a>
  */
 public class MyExtension implements Extension {
+
+    public static int PSB_OBSERVED = 0;
 
     public void observe(@Observes WeldAfterBeanDiscovery abd, BeanManager bm) {
         // register an alternative bean via configurator with priority
@@ -41,5 +44,9 @@ public class MyExtension implements Extension {
             .priority(100)
             .qualifiers(Any.Literal.INSTANCE, Default.Literal.INSTANCE)
             .scope(Dependent.class);
+    }
+
+    public void observeProcessBean(@Observes ProcessSyntheticBean<FooAlternative> psb) {
+        PSB_OBSERVED++;
     }
 }

tests-arquillian/src/test/java/org/jboss/weld/tests/extensions/custombeans/alternative/CustomPrioritizedBeanFiresProcessBeanEventTest.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source
- * Copyright 2017, Red Hat, Inc., and individual contributors
+ * Copyright 2021, Red Hat, Inc., and individual contributors
  * by the @authors tag. See the copyright.txt in the distribution for a
  * full listing of individual contributors.
  *
@@ -14,10 +14,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jboss.weld.tests.alternatives.customBeanPriority;
 
-import javax.enterprise.inject.spi.Extension;
-import javax.inject.Inject;
+package org.jboss.weld.tests.extensions.custombeans.alternative;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -29,30 +27,30 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import javax.enterprise.inject.spi.Extension;
+import javax.inject.Inject;
+
 /**
- * Tests that you can create a custom bean via (Weld)BeanConfigurator and give it a priority hence selecting it.
- * @author <a href="mailto:manovotn@redhat.com">Matej Novotny</a>
+ * Tests that registering a synthetic enabled alternative via {@code Bean<T>} that implements {@code Prioritized} will
+ * fire {@code ProcessBean} event.
  */
 @RunWith(Arquillian.class)
-public class CustomBeanPriorityTest {
+public class CustomPrioritizedBeanFiresProcessBeanEventTest {
 
     @Deployment
     public static JavaArchive createTestArchive() {
-        return ShrinkWrap.create(BeanArchive.class, Utils.getDeploymentNameAsHash(CustomBeanPriorityTest.class))
-            .addClasses(CustomBeanPriorityTest.class, MyExtension.class, PlainFoo.class, FooAlternative.class)
-            .addAsServiceProvider(Extension.class, MyExtension.class);
+        return ShrinkWrap.create(BeanArchive.class, Utils.getDeploymentNameAsHash(CustomPrioritizedBeanFiresProcessBeanEventTest.class))
+                .addClasses(Foo.class, MyExtension.class, FooBean.class)
+                .addAsServiceProvider(Extension.class, MyExtension.class);
     }
 
     @Inject
-    PlainFoo foo;
-
-    @Inject
-    FooAlternative alternative;
+    Foo foo;
 
     @Test
-    public void contextLifecycleEventFiredForPostConstructCallbackActivation() {
-        Assert.assertEquals("bar", alternative.ping());
-        Assert.assertEquals("bar", foo.ping());
-        Assert.assertEquals(1, MyExtension.PSB_OBSERVED);
+    public void testBeanTriggeredEvents() {
+        Assert.assertEquals(2, MyExtension.PB_TRIGGERED);
+        Assert.assertEquals(1, MyExtension.PSB_TRIGGERED);
+        Assert.assertEquals(FooBean.class.getSimpleName(), foo.ping());
     }
 }

tests-arquillian/src/test/java/org/jboss/weld/tests/extensions/custombeans/alternative/Foo.java
@@ -1,0 +1,38 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2021, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.weld.tests.extensions.custombeans.alternative;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class Foo {
+
+    private final String val;
+
+    public Foo() {
+        val = Foo.class.getSimpleName();
+    }
+
+    public Foo(String val) {
+        this.val = val;
+    }
+
+    public String ping() {
+        return val;
+    }
+}

tests-arquillian/src/test/java/org/jboss/weld/tests/extensions/custombeans/alternative/FooBean.java
@@ -1,0 +1,97 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2021, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.weld.tests.extensions.custombeans.alternative;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.inject.Default;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.enterprise.inject.spi.Prioritized;
+
+public class FooBean implements Bean<Foo>, Prioritized {
+    @Override
+    public Class<?> getBeanClass() {
+        return Foo.class;
+    }
+
+    @Override
+    public Set<InjectionPoint> getInjectionPoints() {
+        return Collections.EMPTY_SET;
+    }
+
+    @Override
+    public boolean isNullable() {
+        return false;
+    }
+
+    @Override
+    public Foo create(CreationalContext<Foo> creationalContext) {
+        return new Foo(FooBean.class.getSimpleName());
+    }
+
+    @Override
+    public void destroy(Foo instance, CreationalContext<Foo> creationalContext) {
+    }
+
+    private <T> Set<T> immutableSet(T... items) {
+        Set<T> set = new HashSet<T>();
+        Collections.addAll(set, items);
+        return Collections.unmodifiableSet(set);
+    }
+
+    @Override
+    public Set<Type> getTypes() {
+        return immutableSet(Object.class, Foo.class);
+    }
+
+    @Override
+    public Set<Annotation> getQualifiers() {
+        return immutableSet(Default.Literal.INSTANCE);
+    }
+
+    @Override
+    public Class<? extends Annotation> getScope() {
+        return ApplicationScoped.class;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public Set<Class<? extends Annotation>> getStereotypes() {
+        return Collections.EMPTY_SET;
+    }
+
+    @Override
+    public boolean isAlternative() {
+        return true;
+    }
+
+    @Override
+    public int getPriority() {
+        return 1;
+    }
+}

tests-arquillian/src/test/java/org/jboss/weld/tests/extensions/custombeans/alternative/MyExtension.java
@@ -1,0 +1,42 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2021, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.weld.tests.extensions.custombeans.alternative;
+
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.spi.AfterBeanDiscovery;
+import javax.enterprise.inject.spi.Extension;
+import javax.enterprise.inject.spi.ProcessBean;
+import javax.enterprise.inject.spi.ProcessSyntheticBean;
+
+public class MyExtension implements Extension {
+
+    public static int PB_TRIGGERED = 0;
+    public static int PSB_TRIGGERED = 0;
+
+    public void observeABD(@Observes AfterBeanDiscovery abd) {
+        abd.addBean(new FooBean());
+    }
+
+    public void obsevePB(@Observes ProcessBean<Foo> psb) {
+        PB_TRIGGERED++;
+    }
+
+    public void obsevePSB(@Observes ProcessSyntheticBean<Foo> psb) {
+        PSB_TRIGGERED++;
+    }
+}

rhusar/weld-core

impl/src/main/java/org/jboss/weld/util/Beans.java
@@ -58,6 +58,7 @@ import jakarta.enterprise.inject.spi.Bean;
 import jakarta.enterprise.inject.spi.BeanAttributes;
 import jakarta.enterprise.inject.spi.Extension;
 import jakarta.enterprise.inject.spi.PassivationCapable;
+import jakarta.enterprise.inject.spi.Prioritized;
 import jakarta.inject.Inject;
 
 import org.jboss.weld.annotated.enhanced.EnhancedAnnotated;
@@ -70,6 +71,7 @@ import org.jboss.weld.bean.DecoratorImpl;
 import org.jboss.weld.bean.ForwardingBean;
 import org.jboss.weld.bean.InterceptorImpl;
 import org.jboss.weld.bean.RIBean;
+import org.jboss.weld.bean.WeldBean;
 import org.jboss.weld.bootstrap.api.ServiceRegistry;
 import org.jboss.weld.bootstrap.enablement.ModuleEnablement;
 import org.jboss.weld.injection.FieldInjectionPoint;
@@ -223,16 +225,23 @@ public class Beans {
 
     public static boolean isBeanEnabled(Bean<?> bean, ModuleEnablement enabled) {
         if (bean.isAlternative()) {
+            boolean isEnabled = false;
             if (enabled.isEnabledAlternativeClass(bean.getBeanClass())) {
-                return true;
+                isEnabled = true;
             } else {
                 for (Class<? extends Annotation> stereotype : bean.getStereotypes()) {
                     if (enabled.isEnabledAlternativeStereotype(stereotype)) {
-                        return true;
+                        isEnabled = true;
+                        break;
                     }
                 }
-                return false;
             }
+            // For synthetic enabled alternatives, the ModuleEnablement may not yet be aware of them
+            if (!isEnabled
+                    && ((bean instanceof WeldBean && ((WeldBean<?>) bean).getPriority() != null) || bean instanceof Prioritized)) {
+                isEnabled = true;
+            }
+            return isEnabled;
         } else if (bean instanceof AbstractProducerBean<?, ?, ?>) {
             AbstractProducerBean<?, ?, ?> receiverBean = (AbstractProducerBean<?, ?, ?>) bean;
             return isBeanEnabled(receiverBean.getDeclaringBean(), enabled);

tests-arquillian/src/test/java/org/jboss/weld/tests/alternatives/customBeanPriority/MyExtension.java
@@ -23,6 +23,7 @@ import jakarta.enterprise.inject.Default;
 import jakarta.enterprise.inject.spi.BeanManager;
 import jakarta.enterprise.inject.spi.Extension;
 
+import jakarta.enterprise.inject.spi.ProcessSyntheticBean;
 import org.jboss.weld.bootstrap.event.WeldAfterBeanDiscovery;
 
 /**
@@ -30,6 +31,8 @@ import org.jboss.weld.bootstrap.event.WeldAfterBeanDiscovery;
  * @author <a href="mailto:manovotn@redhat.com">Matej Novotny</a>
  */
 public class MyExtension implements Extension {
+
+    public static int PSB_OBSERVED = 0;
 
     public void observe(@Observes WeldAfterBeanDiscovery abd, BeanManager bm) {
         // register an alternative bean via configurator with priority
@@ -41,5 +44,9 @@ public class MyExtension implements Extension {
             .priority(100)
             .qualifiers(Any.Literal.INSTANCE, Default.Literal.INSTANCE)
             .scope(Dependent.class);
+    }
+
+    public void observeProcessBean(@Observes ProcessSyntheticBean<FooAlternative> psb) {
+        PSB_OBSERVED++;
     }
 }

tests-arquillian/src/test/java/org/jboss/weld/tests/extensions/custombeans/alternative/CustomPrioritizedBeanFiresProcessBeanEventTest.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source
- * Copyright 2017, Red Hat, Inc., and individual contributors
+ * Copyright 2021, Red Hat, Inc., and individual contributors
  * by the @authors tag. See the copyright.txt in the distribution for a
  * full listing of individual contributors.
  *
@@ -14,10 +14,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jboss.weld.tests.alternatives.customBeanPriority;
 
-import jakarta.enterprise.inject.spi.Extension;
-import jakarta.inject.Inject;
+package org.jboss.weld.tests.extensions.custombeans.alternative;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -29,30 +27,30 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import jakarta.enterprise.inject.spi.Extension;
+import jakarta.inject.Inject;
+
 /**
- * Tests that you can create a custom bean via (Weld)BeanConfigurator and give it a priority hence selecting it.
- * @author <a href="mailto:manovotn@redhat.com">Matej Novotny</a>
+ * Tests that registering a synthetic enabled alternative via {@code Bean<T>} that implements {@code Prioritized} will
+ * fire {@code ProcessBean} event.
  */
 @RunWith(Arquillian.class)
-public class CustomBeanPriorityTest {
+public class CustomPrioritizedBeanFiresProcessBeanEventTest {
 
     @Deployment
     public static JavaArchive createTestArchive() {
-        return ShrinkWrap.create(BeanArchive.class, Utils.getDeploymentNameAsHash(CustomBeanPriorityTest.class))
-            .addClasses(CustomBeanPriorityTest.class, MyExtension.class, PlainFoo.class, FooAlternative.class)
-            .addAsServiceProvider(Extension.class, MyExtension.class);
+        return ShrinkWrap.create(BeanArchive.class, Utils.getDeploymentNameAsHash(CustomPrioritizedBeanFiresProcessBeanEventTest.class))
+                .addClasses(Foo.class, MyExtension.class, FooBean.class)
+                .addAsServiceProvider(Extension.class, MyExtension.class);
     }
 
     @Inject
-    PlainFoo foo;
-
-    @Inject
-    FooAlternative alternative;
+    Foo foo;
 
     @Test
-    public void contextLifecycleEventFiredForPostConstructCallbackActivation() {
-        Assert.assertEquals("bar", alternative.ping());
-        Assert.assertEquals("bar", foo.ping());
-        Assert.assertEquals(1, MyExtension.PSB_OBSERVED);
+    public void testBeanTriggeredEvents() {
+        Assert.assertEquals(2, MyExtension.PB_TRIGGERED);
+        Assert.assertEquals(1, MyExtension.PSB_TRIGGERED);
+        Assert.assertEquals(FooBean.class.getSimpleName(), foo.ping());
     }
 }

tests-arquillian/src/test/java/org/jboss/weld/tests/extensions/custombeans/alternative/Foo.java
@@ -1,0 +1,38 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2021, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.weld.tests.extensions.custombeans.alternative;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class Foo {
+
+    private final String val;
+
+    public Foo() {
+        val = Foo.class.getSimpleName();
+    }
+
+    public Foo(String val) {
+        this.val = val;
+    }
+
+    public String ping() {
+        return val;
+    }
+}

tests-arquillian/src/test/java/org/jboss/weld/tests/extensions/custombeans/alternative/FooBean.java
@@ -1,0 +1,98 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2021, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.weld.tests.extensions.custombeans.alternative;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.spi.CreationalContext;
+import jakarta.enterprise.inject.Default;
+import jakarta.enterprise.inject.spi.Bean;
+import jakarta.enterprise.inject.spi.InjectionPoint;
+import jakarta.enterprise.inject.spi.Prioritized;
+
+public class FooBean implements Bean<Foo>, Prioritized {
+    @Override
+    public Class<?> getBeanClass() {
+        return Foo.class;
+    }
+
+    @Override
+    public Set<InjectionPoint> getInjectionPoints() {
+        return Collections.EMPTY_SET;
+    }
+
+    @Override
+    public boolean isNullable() {
+        return false;
+    }
+
+    @Override
+    public Foo create(CreationalContext<Foo> creationalContext) {
+        return new Foo(FooBean.class.getSimpleName());
+    }
+
+    @Override
+    public void destroy(Foo instance, CreationalContext<Foo> creationalContext) {
+    }
+
+    private <T> Set<T> immutableSet(T... items) {
+        Set<T> set = new HashSet<T>();
+        Collections.addAll(set, items);
+        return Collections.unmodifiableSet(set);
+    }
+
+    @Override
+    public Set<Type> getTypes() {
+        return immutableSet(Object.class, Foo.class);
+    }
+
+    @Override
+    public Set<Annotation> getQualifiers() {
+        return immutableSet(Default.Literal.INSTANCE);
+    }
+
+    @Override
+    public Class<? extends Annotation> getScope() {
+        return ApplicationScoped.class;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public Set<Class<? extends Annotation>> getStereotypes() {
+        return Collections.EMPTY_SET;
+    }
+
+    @Override
+    public boolean isAlternative() {
+        return true;
+    }
+
+    @Override
+    public int getPriority() {
+        return 1;
+    }
+}

tests-arquillian/src/test/java/org/jboss/weld/tests/extensions/custombeans/alternative/MyExtension.java
@@ -1,0 +1,42 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2021, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.weld.tests.extensions.custombeans.alternative;
+
+import jakarta.enterprise.event.Observes;
+import jakarta.enterprise.inject.spi.AfterBeanDiscovery;
+import jakarta.enterprise.inject.spi.Extension;
+import jakarta.enterprise.inject.spi.ProcessBean;
+import jakarta.enterprise.inject.spi.ProcessSyntheticBean;
+
+public class MyExtension implements Extension {
+
+    public static int PB_TRIGGERED = 0;
+    public static int PSB_TRIGGERED = 0;
+
+    public void observeABD(@Observes AfterBeanDiscovery abd) {
+        abd.addBean(new FooBean());
+    }
+
+    public void obsevePB(@Observes ProcessBean<Foo> psb) {
+        PB_TRIGGERED++;
+    }
+
+    public void obsevePSB(@Observes ProcessSyntheticBean<Foo> psb) {
+        PSB_TRIGGERED++;
+    }
+}