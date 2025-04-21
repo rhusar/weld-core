manovotn/core

Removal of methods deprecated in new CDI API.

tests-arquillian/src/test/java/org/jboss/weld/tests/annotatedType/observers/RoomsExtension.java
@@ -46,9 +46,10 @@ public class RoomsExtension implements Extension
    private void addRoomAnnotatedTypes(@Observes BeforeBeanDiscovery bbd, BeanManager bm) {
       
       AnnotatedType<Room> wrapped = bm.createAnnotatedType(Room.class);
-      
-      bbd.addAnnotatedType(new RoomAnnotatedTypeWrapper(wrapped, "hall"));
-      bbd.addAnnotatedType(new RoomAnnotatedTypeWrapper(wrapped, "pit"));
+      String hallID = "hall";
+      String pitID = "pit";
+      bbd.addAnnotatedType(new RoomAnnotatedTypeWrapper(wrapped, hallID), hallID);
+      bbd.addAnnotatedType(new RoomAnnotatedTypeWrapper(wrapped, pitID), hallID);
    }
    
    private static class RoomAnnotatedTypeWrapper implements AnnotatedType<Room> {

rhusar/weld-core

tests-arquillian/src/test/java/org/jboss/weld/tests/annotatedType/observers/RoomsExtension.java
@@ -46,9 +46,10 @@ public class RoomsExtension implements Extension
    private void addRoomAnnotatedTypes(@Observes BeforeBeanDiscovery bbd, BeanManager bm) {
       
       AnnotatedType<Room> wrapped = bm.createAnnotatedType(Room.class);
-      
-      bbd.addAnnotatedType(new RoomAnnotatedTypeWrapper(wrapped, "hall"));
-      bbd.addAnnotatedType(new RoomAnnotatedTypeWrapper(wrapped, "pit"));
+      String hallID = "hall";
+      String pitID = "pit";
+      bbd.addAnnotatedType(new RoomAnnotatedTypeWrapper(wrapped, hallID), hallID);
+      bbd.addAnnotatedType(new RoomAnnotatedTypeWrapper(wrapped, pitID), pitID);
    }
    
    private static class RoomAnnotatedTypeWrapper implements AnnotatedType<Room> {